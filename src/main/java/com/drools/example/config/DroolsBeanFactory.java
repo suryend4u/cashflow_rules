0301e8d347c20452576600313472bf49ab48bbdb
Suryendu Das <surdas@WKMZTF1DD3A1.global.publicisgroupe.net>
2023-02-27 17:16:17 +0000

    Printing drools file

--- src/main/java/com/drools/example/config/DroolsBeanFactory.java
+++ src/main/java/com/drools/example/config/DroolsBeanFactory.java
@@ -1,5 +1,6 @@
 package com.drools.example.config;
 
+
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.KieRepository;
@@ -7,11 +8,21 @@ import org.kie.api.builder.ReleaseId;
 import org.kie.api.io.Resource;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+/*import org.drools.decisiontable.DecisionTableProviderImpl;
+import org.kie.internal.builder.DecisionTableConfiguration;
+import org.kie.internal.builder.KnowledgeBuilderFactory;*/
 
 
 public class DroolsBeanFactory {
 
     public KieSession getKieSession(Resource dt) {
+/*        DecisionTableConfiguration configuration =
+                KnowledgeBuilderFactory.newDecisionTableConfiguration();
+        DecisionTableProviderImpl decisionTableProvider = new DecisionTableProviderImpl();
+        String drl = decisionTableProvider.loadFromResource(dt, configuration);
+        System.out.println("*********Drools file start************");
+        System.out.println(drl);
+        System.out.println("*********Drools file end************");*/
         KieServices kieServices = KieServices.Factory.get();
         KieFileSystem kieFileSystem = kieServices.newKieFileSystem()
                 .write(dt);
@@ -22,4 +33,5 @@ public class DroolsBeanFactory {
         KieContainer kieContainer = kieServices.newKieContainer(krDefaultReleaseId);
         return kieContainer.newKieSession();
     }
+
 }
